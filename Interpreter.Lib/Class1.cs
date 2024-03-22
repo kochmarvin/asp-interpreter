ebfc990d96ba38c2f087bfbe36a3d02ef92fa6e5
Marvin Koch <marvin.info@gmx.at>
2024-03-22 12:43:52 +0100

    init

---
+++ Interpreter.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Interpreter.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Interpreter.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Interpreter.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
