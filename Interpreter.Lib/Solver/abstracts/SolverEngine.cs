c265cacf243e93a701f806e7dd4eb0bf66b9e99b
kochmarvin <marvin.info@gmx.at>
2024-04-24 21:07:55 +0200

    :zap: solver engine

---
+++ Interpreter.Lib/Solver/abstracts/SolverEngine.cs
@@ -0,0 +1,13 @@
+using Interpreter.Lib.Results.Objects.Atoms;
+using Interpreter.Lib.Solver.Interfaces;
+
+namespace Interpreter.Lib.Solver.Abstracts;
+
+public abstract class SolverEngine(IPreparer preparer, ITransformer transformer, ISolver solver)
+{
+  public IPreparer Preparer { get; } = preparer;
+  public ITransformer Transformer { get; } = transformer;
+  public ISolver Solver { get; } = solver;
+
+  public abstract List<List<Atom>>? Execute();
+}
