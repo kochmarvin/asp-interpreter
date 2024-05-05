87ac2228814d4f478de46db53df4347c00aad274
Julia Tuka <julia.tuka@aon.at>
2024-05-05 19:10:14 +0200

    added sat engine tests

--- Interpreter.Tests/SatEngine/SatEngine.spec.cs
+++ Interpreter.Tests/SatEngine/SatEngine.spec.cs
@@ -22,8 +22,6 @@ public class SatEngineTests
 
     List<List<Atom>> results = satEngine.Execute();
 
-    List<List<Atom>> excpeted = [[], [new Atom("marvin", [])]];
-
     Assert.IsTrue(AreEquivalent(results, obj.Expected), "Results and expected lists are not equivalent.");
   }
 
@@ -88,7 +86,6 @@ public class SatEngineTests
      ]
    );
 
-    // ist noch nicht correct, gibt verschiedene antworten aus
     // yield return new SatEngineResult(
     //   "choice_head.lp",
     //   [
@@ -165,6 +162,232 @@ public class SatEngineTests
       [
         [new Atom("b", [])],
         [new Atom("x", []), new Atom("a", [])],
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "blocks.lp",
+      [
+        [new Atom("stacked", [new Variable("b")]),
+        new Atom("stacked", [new Variable("a")]), 
+        new Atom("puton", [new Variable("d"), new Variable("d")]),
+        new Atom("puton", [new Variable("d"), new Variable("a")]),
+        new Atom("puton", [new Variable("a"), new Variable("d")]),
+        new Atom("puton", [new Variable("a"), new Variable("a")]),
+        new Atom("on", [new Variable("b"), new Variable("c")]),
+        new Atom("on", [new Variable("a"), new Variable("b")]),
+        new Atom("handempty", []),
+        new Atom("clear", [new Variable("d")]), 
+        new Atom("clear", [new Variable("a")])]
+      ]
+    );
+
+    // yield return new SatEngineResult(
+    //   "multichoice_body.lp",
+    //   [
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("student", [new Variable("marvin")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("student", [new Variable("marvin")]), new Atom("informatiker", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("julia")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("student", [new Variable("marvin")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("student", [new Variable("marvin")]), new Atom("informatiker", [new Variable("julia")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("student", [new Variable("marvin")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("student", [new Variable("marvin")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("informatiker", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("informatiker", [new Variable("julia")]), new Atom("student", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("student", [new Variable("marvin")]), new Atom("informatiker", [new Variable("julia")])],
+    //     [new Atom("mensch", [new Variable("marvin")]), new Atom("mensch", [new Variable("julia")]), new Atom("informatiker", [new Variable("marvin")]), new Atom("student", [new Variable("marvin")]), new Atom("informatiker", [new Variable("julia")]), new Atom("student", [new Variable("julia")])]
+    //   ]
+    // );
+    
+    // da fickt die reihenfolge der AS wieder, ab und zu kommts durch 
+    yield return new SatEngineResult(
+      "teaches.lp",
+      [
+        [
+        new Atom("teaches", [new Variable("tom"), new Variable("logic")]),
+        new Atom("teaches", [new Variable("sam"), new Variable("java")]),
+        new Atom("teaches", [new Variable("sam"), new Variable("c")]),
+        new Atom("teaches", [new Variable("bob"), new Variable("ai")]),
+        new Atom("some_course", [new Variable("tom")]),
+        new Atom("some_course", [new Variable("sam")]),
+        new Atom("some_course", [new Variable("bob")]),
+        new Atom("member", [new Variable("tom"), new Variable("cs")]),
+        new Atom("member", [new Variable("sam"), new Variable("cs")]),
+        new Atom("member", [new Variable("bob"), new Variable("cs")]),
+        new Atom("likes", [new Variable("tom"), new Variable("logic")]),
+        new Atom("likes", [new Variable("tom"), new Variable("ai")]),
+        new Atom("likes", [new Variable("sam"), new Variable("java")]), 
+        new Atom("likes", [new Variable("sam"), new Variable("c")]), 
+        new Atom("likes", [new Variable("bob"), new Variable("java")]),
+        new Atom("likes", [new Variable("bob"), new Variable("ai")]),
+        new Atom("course", [new Variable("logic"), new Variable("cs")]),
+        new Atom("course", [new Variable("java"), new Variable("cs")]),
+        new Atom("course", [new Variable("c"), new Variable("cs")]),
+        new Atom("course", [new Variable("ai"), new Variable("cs")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("java")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("c")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("ai")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("logic")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("ai")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("logic")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("java")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("c")]),
+        ],
+        [
+        new Atom("teaches", [new Variable("tom"), new Variable("logic")]),
+        new Atom("teaches", [new Variable("tom"), new Variable("ai")]),
+        new Atom("teaches", [new Variable("sam"), new Variable("c")]),
+        new Atom("teaches", [new Variable("bob"), new Variable("java")]),
+        new Atom("some_course", [new Variable("tom")]),
+        new Atom("some_course", [new Variable("sam")]),
+        new Atom("some_course", [new Variable("bob")]),
+        new Atom("member", [new Variable("tom"), new Variable("cs")]),
+        new Atom("member", [new Variable("sam"), new Variable("cs")]),
+        new Atom("member", [new Variable("bob"), new Variable("cs")]),
+        new Atom("likes", [new Variable("tom"), new Variable("logic")]),
+        new Atom("likes", [new Variable("tom"), new Variable("ai")]),
+        new Atom("likes", [new Variable("sam"), new Variable("java")]), 
+        new Atom("likes", [new Variable("sam"), new Variable("c")]), 
+        new Atom("likes", [new Variable("bob"), new Variable("java")]),
+        new Atom("likes", [new Variable("bob"), new Variable("ai")]),
+        new Atom("course", [new Variable("logic"), new Variable("cs")]),
+        new Atom("course", [new Variable("java"), new Variable("cs")]),
+        new Atom("course", [new Variable("c"), new Variable("cs")]),
+        new Atom("course", [new Variable("ai"), new Variable("cs")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("java")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("c")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("logic")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("java")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("ai")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("logic")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("c")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("ai")]),
+        ],
+        [
+        new Atom("teaches", [new Variable("tom"), new Variable("logic")]),
+        new Atom("teaches", [new Variable("sam"), new Variable("c")]),
+        new Atom("teaches", [new Variable("bob"), new Variable("java")]),
+        new Atom("teaches", [new Variable("bob"), new Variable("ai")]),
+        new Atom("some_course", [new Variable("tom")]),
+        new Atom("some_course", [new Variable("sam")]),
+        new Atom("some_course", [new Variable("bob")]),
+        new Atom("member", [new Variable("tom"), new Variable("cs")]),
+        new Atom("member", [new Variable("sam"), new Variable("cs")]),
+        new Atom("member", [new Variable("bob"), new Variable("cs")]),
+        new Atom("likes", [new Variable("tom"), new Variable("logic")]),
+        new Atom("likes", [new Variable("tom"), new Variable("ai")]),
+        new Atom("likes", [new Variable("sam"), new Variable("java")]), 
+        new Atom("likes", [new Variable("sam"), new Variable("c")]), 
+        new Atom("likes", [new Variable("bob"), new Variable("java")]),
+        new Atom("likes", [new Variable("bob"), new Variable("ai")]),
+        new Atom("course", [new Variable("logic"), new Variable("cs")]),
+        new Atom("course", [new Variable("java"), new Variable("cs")]),
+        new Atom("course", [new Variable("c"), new Variable("cs")]),
+        new Atom("course", [new Variable("ai"), new Variable("cs")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("java")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("c")]),
+        new Atom("-teaches", [new Variable("tom"), new Variable("ai")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("logic")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("java")]),
+        new Atom("-teaches", [new Variable("sam"), new Variable("ai")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("logic")]),
+        new Atom("-teaches", [new Variable("bob"), new Variable("c")]),
+        ]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "edge.lp",
+      [
+        [
+          new Atom("edge", [new Variable("c"), new Variable("d")]), 
+          new Atom("edge", [new Variable("b"), new Variable("d")]), 
+          new Atom("edge", [new Variable("b"), new Variable("c")]), 
+          new Atom("edge", [new Variable("a"), new Variable("c")]), 
+          new Atom("edge", [new Variable("a"), new Variable("b")]), 
+          new Atom("color", [new Variable("red")]), 
+          new Atom("color", [new Variable("green")]), 
+          new Atom("color", [new Variable("blue")]), 
+        ]
+      ]
+    );
+    
+    yield return new SatEngineResult(
+      "family_relations.lp",
+      [
+        [
+          new Atom("parent", [new Variable("charlie"), new Variable("emma")]), 
+          new Atom("parent", [new Variable("bob"), new Variable("charlie")]), 
+          new Atom("parent", [new Variable("bob"), new Variable("alice")]), 
+          new Atom("parent", [new Variable("alice"), new Variable("david")]), 
+          new Atom("ancestor", [new Variable("charlie"), new Variable("emma")]), 
+          new Atom("ancestor", [new Variable("bob"), new Variable("emma")]), 
+          new Atom("ancestor", [new Variable("bob"), new Variable("david")]), 
+          new Atom("ancestor", [new Variable("bob"), new Variable("charlie")]), 
+          new Atom("ancestor", [new Variable("bob"), new Variable("alice")]), 
+          new Atom("ancestor", [new Variable("alice"), new Variable("david")]), 
+        ]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "negations.lp",
+      [
+        [new Atom("p", [])], 
+        [new Atom("q", [])]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "assign.lp",
+      [
+        [new Atom("sum", [new Number(1), new Number(2), new Number(3)])]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "happy.lp",
+      [
+        [
+          new Atom("unhappy", [new Variable("bob")]), 
+          new Atom("sad", [new Variable("alice")]), 
+          new Atom("reasontobeunhappy", [new Variable("bob")]),
+          new Atom("reasontobehappy", [new Variable("alice")]),
+          new Atom("happy", [new Variable("alice")])
+        ]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "arithop_times.lp",
+      [
+        [new Atom("sum", [new Number(5)]), new Atom("result", [new Number(100)]), new Atom("product", [new Number(20)])]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "arithop_div.lp",
+      [
+        [new Atom("two", [new Number(4)]), new Atom("result", [new Number(5)]), new Atom("one", [new Number(20)])]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "arithop_minus.lp",
+      [
+        [new Atom("result", [new Number(16)]), new Atom("number_two", [new Number(4)]), new Atom("number_one", [new Number(20)])]
+      ]
+    );
+
+    yield return new SatEngineResult(
+      "arithop_plus.lp",
+      [
+        [new Atom("result", [new Number(24)]), new Atom("num_two", [new Number(4)]), new Atom("num_one", [new Number(20)])]
       ]
     );
 
