ff9c8a23c1fcfd9fcdcdf29cb260aa5be4a94735
kochmarvin <marvin.info@gmx.at>
2024-05-22 09:52:19 +0200

    :alien: comments

--- Interpreter.Lib/Graph/Abstracts/DependecyGraph.cs
+++ Interpreter.Lib/Graph/Abstracts/DependecyGraph.cs
@@ -1,10 +1,11 @@
+namespace Interpreter.Lib.Graph;
+
 using Interpreter.Lib.Results.Objects.Rule;
 using Microsoft.FSharp.Core;
 
-namespace Interpreter.Lib.Graph;
 
 /// <summary>
-/// Genrates the dependecy graph and makes an SCC graph out of it
+/// 
 /// </summary>
 public abstract class DependencyGraph
 {
@@ -12,56 +13,72 @@ public abstract class DependencyGraph
   private LiteralVisitor<int> orderVisitor;
   private AddToGraphVisitor addToGraphVisitor;
 
+  /// <summary>
+  /// 
+  /// </summary>
   public List<ProgramRule> Program
   {
     get
     {
-      return program;
+      return this.program;
     }
 
     set
     {
-      program = value ?? throw new ArgumentNullException(nameof(Program) + "Is not supposed to be null");
-    }
-  }
-
-  public LiteralVisitor<int> OrderVisitor
-  {
-    get
-    {
-      return orderVisitor;
-    }
-
-    private set
-    {
-      orderVisitor = value ?? throw new ArgumentNullException(nameof(OrderVisitor) + "Is not supposed to be null");
-    }
-  }
-
-  public AddToGraphVisitor AddToGraphVisitor
-  {
-    get
-    {
-      return addToGraphVisitor;
-    }
-
-    private set
-    {
-      addToGraphVisitor = value ?? throw new ArgumentNullException(nameof(AddToGraphVisitor) + "Is not supposed to be null");
+      this.program = value ?? throw new ArgumentNullException(nameof(this.Program) + "Is not supposed to be null");
     }
   }
 
   /// <summary>
-  /// Constructor which will order the rules in a certain way
+  /// 
   /// </summary>
-  /// <param name="program">The progra which you want the graph of</param>
-  public DependencyGraph(List<ProgramRule> program, LiteralVisitor<int> orderVisitor, AddToGraphVisitor addToGraphVisitor)
+  public LiteralVisitor<int> OrderVisitor
   {
-    Program = program;
-    OrderVisitor = orderVisitor;
-    AddToGraphVisitor = addToGraphVisitor;
+    get
+    {
+      return this.orderVisitor;
+    }
+
+    private set
+    {
+      this.orderVisitor = value ?? throw new ArgumentNullException(nameof(this.OrderVisitor) + "Is not supposed to be null");
+    }
   }
 
+  /// <summary>
+  /// 
+  /// </summary>
+  public AddToGraphVisitor AddToGraphVisitor
+  {
+    get
+    {
+      return this.addToGraphVisitor;
+    }
+
+    private set
+    {
+      this.addToGraphVisitor = value ?? throw new ArgumentNullException(nameof(this.AddToGraphVisitor) + "Is not supposed to be null");
+    }
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="program"></param>
+  /// <param name="orderVisitor"></param>
+  /// <param name="addToGraphVisitor"></param>
+  public DependencyGraph(List<ProgramRule> program, LiteralVisitor<int> orderVisitor, AddToGraphVisitor addToGraphVisitor)
+  {
+    this.Program = program;
+    this.OrderVisitor = orderVisitor;
+    this.AddToGraphVisitor = addToGraphVisitor;
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="program"></param>
+  /// <returns></returns>
   public abstract DependencyGraph CreateNewGraphInstance(List<ProgramRule> program);
 
   /// <summary>
